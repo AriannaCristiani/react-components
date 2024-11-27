5a9b248b0baef5afd5d2ea6c365dc7e4a79e83f5
AriannaCrisriani <ariannacristiani@yahoo.it>
2024-11-27 15:19:09 +0100

    creazione header con rispettivo file css e importazione corretta

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-
+import Header from './components/Header/Header'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-      
+      <Header></Header>
     </>
   )
 }
