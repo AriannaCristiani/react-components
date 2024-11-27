b9b2e167d7cb64a87533c51554082ef9131e38e5
AriannaCrisriani <ariannacristiani@yahoo.it>
2024-11-27 15:49:11 +0100

    ceazione card nel Main e importazione in app.jsx

---
+++ src/components/Main.jsx
@@ -0,0 +1,22 @@
+
+
+export default function Main() {
+    return (
+        <main>
+            <section>
+                <div className="container">
+                    <div className="row">
+                        <div className="col">
+                            <div className="card">
+                                <img src="../../600x400.png" alt="" />
+                                <h3 className="card-title">Titolo del Post</h3>
+                                <p className="card-body">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Libero adipisci consectetur dolor suscipit expedita non delectus laudantium sint aliquid excepturi?</p>
+                                <button className="btn">LEGGI DI PIU'</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </main>
+    )
+}
